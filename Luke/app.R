a8ab2ad07b2d773eda4c72bff8d502732854de7c
lukemdcode <lukeconroy@MacBook-Pro.local>
2022-01-31 16:30:50 +0000

    Improved IMD dashboard

--- Luke/app.R
+++ Luke/app.R
@@ -12,24 +12,40 @@ theme_update(axis.title = element_text(),
 input_directory <- "/Users/lukeconroy/OneDrive/Masters_UK/HDS/Semester_2/Data_Challenge/Raw_Data/Official_data/Analysis Dataset/"
 setwd(input_directory)
 
+#----------------------------------------------------------------------------------------#
+
 # import datasets from csv and tidy
-gpLevelData <- as_tibble(read.csv("GP_corticosterioid_prescriptions.csv"))  #not using magrittr pipe as ESS on Emacs doesnt recognise
-gpLevelData <- mutate(gpLevelData, year = as.numeric(str_sub(date, 1, 4)))
+## gpLevelData <- as_tibble(read.csv("GP_corticosterioid_prescriptions.csv"))  #not using magrittr pipe as ESS on Emacs doesnt recognise
+## gpLevelData <- mutate(gpLevelData, year = as.numeric(str_sub(date, 1, 4)))
+
+## import and tidy
+gpLevelData <- as_tibble(read.csv("GP_corticosterioid_prescriptions.csv")) %>%
+    mutate(year = as.numeric(str_sub(date, 1, 4)), month = as.numeric(str_sub(date, 6, 7)), date=as.Date(date)) %>%
+    rename(id = X)
+
+yearChoices <-
+    unique(gpLevelData$year)
+
+## put a(n) (invisible) struture on the data
+gpLevelDataGrouped <- group_by(gpLevelData, year, month, deprivation_decile, date)
+gpLevelDataPresentation <- summarise_at(gpLevelDataGrouped, .vars=vars(items), .funs=list(sum))
+
 ccgLevelData <- as_tibble(read.csv("CCG_corticosterioid_prescriptions.csv"))
 ccgLevelData <- mutate(ccgLevelData, year = as.numeric(str_sub(date, 1, 4)))
 yearChoices <- unique(gpLevelData$year)
+
+#------------------------------------------------------------------------------------------#
 
 # page description
 ui <- fluidPage(
   
   #drop down input to select dataset
-  selectInput("inputDataset", "Select dataset", choices=c("gpLevelData", "ccgLevelData", "regionalLevelData")),
+  selectInput("inputDataset", "Select dataset", choices=c("gpLevelDataPresentation", "ccgLevelData", "regionalLevelData")),
   
   #slider input to select year
-  sliderInput("inputYear", "Select Year", min=min(yearChoices), max=max(yearChoices),
-                                                  value=min(yearChoices), step=1),
+  sliderInput("inputYear", "Select Year", min=min(yearChoices), max=max(yearChoices), value=min(yearChoices), step=1),
   
-  #plot output for graph
+  #output element for graph
   plotOutput("deprivationPlot")
   
 )
@@ -37,18 +53,30 @@ ui <- fluidPage(
 # page functionality
 server <- function(input, output, session) {
   
-  # reactive object for the working dataset to reduce reloading
-  currentDataset <- reactive({
-    rawDs <- get((input$inputDataset)) #defaults to searching the calling namespace
-    filter(rawDs, year == input$inputYear)
+# reactive object for the working dataset to reduce reloading
+currentDataset <- reactive({
+    rawDS <- get((input$inputDataset)) #defaults to searching the calling namespace
+    filter(rawDS, year==input$inputYear)
+    })
+
+## render the plot and assign to the output element
+output$deprivationPlot <- renderPlot({
+    ggplot(data=currentDataset()) +
+   geom_col(mapping=aes(x=date, y=items, fill=deprivation_decile)) +
+labs(title = "Systemic Corticosteroid Prescpriptions by IMD", caption = "Source: OpenPrescribing.net, EBM Data Lab, University of Oxford, 2017", x = "Month", y = "Items Prescribed") +
+   scale_x_date(date_labels="%b",
+                date_breaks = "1 month") +
+   theme(panel.grid.major.x = element_blank()) +
+   scale_colour_fivethirtyeight()
+     })
     
-  })
-  
-  # deprivationPlot: render a plot and assign to the output element
-  output$deprivationPlot <- renderPlot({
-    cleanedData <- filter(currentDataset(), is.na(items) == FALSE )
-    ggplot(data=cleanedData) + geom_bar(mapping = aes(x=deprivation_decile, y=items), stat="identity") + scale_colour_fivethirtyeight()
-  })
+    
+# render the plot and assign to the output element
+## output$deprivationPlot <- renderPlot({
+##      # cleaned_data <-  filter(currentDataset(), is.na(items) == FALSE )
+##       ggplot(data=currentDataset()) + geom_bar(mapping = aes(x=deprivation_decile, y=items), stat="identity"    ) + scale_colour_fivethirtyeight()
+      
+##   })
   
 }
 
--- Luke/plot_practise.R
+++ Luke/plot_practise.R
@@ -13,19 +13,27 @@ setwd(input_directory)
 
 ## import and tidy
 gpLevelData <- as_tibble(read.csv("GP_corticosterioid_prescriptions.csv")) %>%
-    mutate(year = as.numeric(str_sub(date, 1, 4)), month = as.numeric(str_sub(date, 6, 7))) %>%
+    mutate(year = as.numeric(str_sub(date, 1, 4)), month = as.numeric(str_sub(date, 6, 7)), date=as.Date(date)) %>%
     rename(id = X)
 
-yearChoices <- unique(gpLevelData$year)
+yearChoices <-
+    unique(gpLevelData$year)
 
 ## put a(n) (invisible) struture on the data
-gpLevelDataGrouped <- group_by(gpLevelData, year, month, deprivation_decile)
+gpLevelDataGrouped <- group_by(gpLevelData, year, month, deprivation_decile, date)
 
 ## summarise by summing items in the grouped tibble
 itemsYearMonthDD <- summarise_at(gpLevelDataGrouped, .vars=vars(items), .funs=list(sum))
+itemsYearMonthDD <- mutate(itemsYearMonthDD)
 
 #plot
-ggplot(data=filter(itemsYearMonthDD, year==2017)) + geom_col(mapping=aes(x=month, y=items, fill=deprivation_decile), stat="identity")
+ggplot(data=filter(itemsYearMonthDD, year==2017)) +
+    geom_col(mapping=aes(x=date, y=items, fill=deprivation_decile)) +
+    labs(title = "Systemic Corticosteroid Prescpriptions by IMD", caption = "Source: OpenPrescribing.net,     EBM DataLab, University of Oxford, 2017", x = "Month", y = "Items Prescribed") +
+    scale_x_date(date_labels="%b",
+                 date_breaks = "1 month") +
+    theme(panel.grid.major.x = element_blank()) +
+  scale_colour_fivethirtyeight()
 
 
 ## ccg data
